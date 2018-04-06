2ff37c569f8b21e8e1d56e1e527f8b742f21b67c
Unknown <gunter.mhertz@gmail.com>
2018-04-06 13:57:04 -0300

    Create project

---
+++ Beacon/Beacon/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Beacon
+//
+//  Created by Günter Hertz on 06/04/18.
+//  Copyright © 2018 Günter Hertz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
