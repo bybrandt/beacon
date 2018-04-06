15b4c1e717e8d1c655b3e08412da7ddf5f9d4ad5
Unknown <gunter.mhertz@gmail.com>
2018-04-06 14:09:48 -0300

    Create models

---
+++ Beacon/Beacon/Models/Store.swift
@@ -0,0 +1,13 @@
+//
+//  Store.swift
+//  Beacon
+//
+//  Created by Günter Hertz on 06/04/18.
+//  Copyright © 2018 Günter Hertz. All rights reserved.
+//
+
+import UIKit
+
+class Store: NSObject {
+
+}
